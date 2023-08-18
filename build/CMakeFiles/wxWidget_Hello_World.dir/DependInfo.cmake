13ed692195bd5665671ef0ffac2ce441d11a2cbb
Digital1O1 <ChristopherTa89@gmail.com>
2023-08-17 19:14:32 -0500

    first push

---
+++ build/CMakeFiles/wxWidget_Hello_World.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ctnano/Desktop/Coding_Folder/wxWidget_Hello_World/main.cpp" "/home/ctnano/Desktop/Coding_Folder/wxWidget_Hello_World/build/CMakeFiles/wxWidget_Hello_World.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ctnano/wxWidgets/include"
+  "/home/ctnano/wxWidgets/buildgtk/lib/wx/include/gtk3-unicode-3.3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/wxWidget_Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wxWidget_Hello_World.dir/main.cpp.o"
+  "wxWidget_Hello_World"
+  "wxWidget_Hello_World.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wxWidget_Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
